30b7649eb380b16cf5aa071df96463942acd0208
alimustofa-am <alimustofa837@gmail.com>
2021-07-18 22:33:29 +0700

    changing Book's plain constructor to class

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
   addBookToLibrary();
 });
 
-function Book(title, author, years, pages, read, id) {
-  this.title = title;
-  this.author = author;
-  this.year = years;
-  this.pages = pages;
-  this.read = read;
-  this.id = id;
+class Book {
+  constructor(title, author, years, pages, read, id) {
+    this.title = title;
+    this.author = author;
+    this.years = years;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
+  }
 }
 
 function addBookToLibrary() {
@@ -91,39 +93,14 @@ function kerangkaElemenBuku(buku) {
 }
 
 function removeTheBook(book) {
-  // const cards = Array.from(document.querySelectorAll('.card'));
-  // const [getCard] = cards.filter((card) => card['bookId'] === id);
-  // const bookIndex = myLibrary.indexOf(getCard);
-
-  // const removeButtton = getCard.querySelector('.remove-button');
-  // removeButtton.addEventListener('click', (e) => {
-  //   myLibrary.splice(bookIndex, 1);
-  //   e.target.parentElement.remove();
-  //   localStorage.setItem(storageKey, JSON.stringify(myLibrary));
-  // });
-
-  // const cards = Array.from(document.querySelectorAll('.card'));
-  // console.log(cards);
-
   const removeButtton = book.querySelector('.remove-button');
   removeButtton.addEventListener('click', () => {
-    // console.log(e.target.parentElement);
-    // console.log(book);
-    // console.log(book.dataset.indexNumber);
-    // console.log(myLibrary.indexOf(book));
-    // console.log(myLibrary);
-    // console.log(book.dataset.indexNumber);
     const [tests] = myLibrary.filter((test) => book['bookId'] === test.id);
     const bookIndex = myLibrary.indexOf(tests);
     myLibrary.splice(bookIndex, 1);
     book.remove();
-    // console.log(myLibrary);
     localStorage.setItem(storageKey, JSON.stringify(myLibrary));
-    // console.log(e.target.parentElement.dataset.indexNumber);
   });
-
-  // console.log(book);
-  // console.log(book.dataset.indexNumber);
 }
 
 function changeReadStatus(book) {
@@ -146,62 +123,7 @@ function changeReadStatus(book) {
 }
 
 function displayTheBook() {
-  // for (const [index, book] of myLibrary.entries()) {
   for (let i = 0; i < myLibrary.length; i++) {
     kerangkaElemenBuku(myLibrary[i]);
-    // kerangkaElemenBuku(book);
-    // console.log(card[index]);
-    // const container = document.getElementById('container');
-    // const card = document.createElement('div');
-    // const title = document.createElement('p');
-    // const author = document.createElement('p');
-    // const year = document.createElement('p');
-    // const pages = document.createElement('p');
-    // const remove = document.createElement('button');
-    // const read = document.createElement('button');
-
-    // card.classList.add('card');
-    // remove.classList.add('remove-button');
-    // read.classList.add('read-button');
-    // card.setAttribute('data-index-number', `${index}`);
-
-    // title.textContent = `Judul Buku ${book.title}`;
-    // author.textContent = `Penulis Buku ${book.author}`;
-    // year.textContent = `Tahun Diterbitkan ${book.year}`;
-    // pages.textContent = `Jumlah Halaman ${book.pages}`;
-
-    // if (book.read) {
-    //   card.style.border = '1px solid green';
-    //   read.textContent = 'Belum selesai';
-    // } else {
-    //   card.style.border = '1px solid red';
-    //   read.textContent = 'Sudah selesai';
-    // }
-
-    // remove.textContent = 'Hapus';
-
-    // card.append(title, author, year, pages, remove, read);
-    // container.appendChild(card);
   }
-
-  // const cardIndex = card.getAttribute('data-index-number');
-  // remove.addEventListener('click', (e) => {
-  //   myLibrary.splice(cardIndex, 1);
-  //   e.target.parentElement.remove();
-  // });
-
-  // read.addEventListener('click', () => {
-  //   const readStatus = myLibrary[cardIndex].read;
-  //   if (readStatus) {
-  //     myLibrary[cardIndex].read = false;
-
-  //     card.style.border = '1px solid red';
-  //     read.textContent = 'Sudah selesai';
-  //   } else {
-  //     myLibrary[cardIndex].read = true;
-
-  //     card.style.border = '1px solid green';
-  //     read.textContent = 'Belum selesai';
-  //   }
-  // });
 }
